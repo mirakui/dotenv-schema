7009c27eccd5cb18345465eba0b650427ca96300
Issei Naruta <naruta@cookpad.com>
2013-10-25 23:16:51 +0900

    Add validator

--- lib/env_schema/schema.rb
+++ lib/env_schema/schema.rb
@@ -1,6 +1,7 @@
 require 'yaml'
 
 module EnvSchema
+  class ValidationError < StandardError; end
   class Schema < Hash
     def initialize(hash={})
       self.replace hash if hash
@@ -10,7 +11,18 @@ module EnvSchema
       new YAML.load(File.read(file))
     end
 
-    def validate
+    def validate(env=ENV)
+      each do |key, value|
+        if has_key?(key)
+          if !env['allow_empty_string'] && env[key].nil?
+            raise ValidationError, "ENV['#{key}'] must not be empty string"
+          end
+          if !env['allow_not_exists'] && !env.has_key?(key)
+            raise ValidationError, "ENV['#{key}'] must exist"
+          end
+        end
+      end
+      true
     end
   end
 end
--- spec/lib/env_schema/schema_spec.rb
+++ spec/lib/env_schema/schema_spec.rb
@@ -17,7 +17,26 @@ KEY3:
     expect(schema).to eq({
 'KEY1' => { 'allow_empty_string' => true, 'allow_not_exists' => true },
 'KEY2' => nil,
-'KEY3' => { 'allow_empty_string' => false },
+'KEY3' => { 'allow_empty_string' => false }
     })
+  end
+
+  it do
+    expect {
+      schema.validate({
+        'KEY1' => 'VALUE1',
+        'KEY2' => 'VALUE2',
+        'KEY3' => 'VALUE3'
+      })
+    }.not_to raise_error
+  end
+
+  it do
+    expect {
+      schema.validate({
+        'KEY1' => 'VALUE1',
+        'KEY3' => 'VALUE3'
+      })
+    }.to raise_error(EnvSchema::ValidationError)
   end
 end
